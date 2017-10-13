bfa5670450349e172fde87dc7b0fcf4df02e37a6
Siomara Jimenez <svjlopez@gmail.com>
2017-10-13 17:51:16 +0000

    Updated about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     
+    #Controller#action
+    #This is the controller and the action is home
     def home
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     
     #root is pointing to homepage
+    #Go to the pages controller and execute the home action
     root to: 'pages#home'
     
     get 'about', to: 'pages#about'
