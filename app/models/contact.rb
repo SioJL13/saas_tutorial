8c6ce7c8fa8a43690517625125e11eab1681e5fe
Siomara Jimenez <svjlopez@gmail.com>
2017-10-18 17:22:52 +0000

    Added form validations to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
 class Contact < ActiveRecord::Base
     #Revisa la BD (schema) para ver que atributos debe de contener la clase
+    #Blueprint principal, aqui se pueden hacer validaciones
+    
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
+    
 end
