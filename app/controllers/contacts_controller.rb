de89f931f3fd23ec3a907dd6d81d3705117baf64
Siomara Jimenez <svjlopez@gmail.com>
2017-10-20 16:15:41 +0000

    Added contact form mailer view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,11 @@ class ContactsController < ApplicationController
     def create
         @contact =  Contact.new(contact_params)
         if @contact.save
-            flash[:success] = "Contact saved"
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body =  params[:contact][:comments]
+            ContactMailer.contact_email(name,email,body).deliver
+            flash[:success] = "Message sent"
             redirect_to new_contact_path
         else
             #the errors came in an array
